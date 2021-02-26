90e2ef658ff27afe2c7fafc08f8c2fac848f3191
DragosPancescu <49004332+DragosPancescu@users.noreply.github.com>
2021-02-26 18:32:12 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,14 +6,15 @@ USERNAME = 'adming' # wordpress username
 PASSWORD = 'admin' # wordpress password
 driver = webdriver.Chrome(PATH)
 
-# Loading my localhost server that contains the neve theme
-driver.get('http://localhost/dragos/wp-admin/customize.php?theme=neve&return=http%3A%2F%2Flocalhost%2Fdragos%2Fwp-admin%2Fthemes.php')
+if __name__ == '__main__':
+  # Loading my localhost server that contains the neve theme
+  driver.get('http://localhost/dragos/wp-admin/customize.php?theme=neve&return=http%3A%2F%2Flocalhost%2Fdragos%2Fwp-admin%2Fthemes.php')
 
-# Login to the theme
-login_function(USERNAME, PASSWORD, driver)
+  # Login to the theme
+  login_function(USERNAME, PASSWORD, driver)
 
-# Open the container panel
-open_container(driver)
+  # Open the container panel
+  open_container(driver)
 
-# Test container width
-test_container_width(driver)
+  # Test container width
+  test_container_width(driver)  
